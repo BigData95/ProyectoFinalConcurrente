32f748e6cd9fa666e309425ab2f8d71e110a8fa0
BigData95 <flakeynavi@gmail.com>
2018-07-15 15:28:32 -0500

    Preguntar a San Miguel

--- Prueba1/src/ProyectoFinalProcesos.c
+++ Prueba1/src/ProyectoFinalProcesos.c
@@ -1,30 +1,21 @@
-/*
- ============================================================================
- Name        : Prueba1.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello MPI World in C 
- ============================================================================
- */
+
 #include <time.h>
 #include <stdio.h>
 #include <string.h>
 
 #include "mpi.h"
 
+
 #define horaIncioDia 540  //9hrs
 #define horaApertura 600  //10hrs
 #define horaFinDia 1380   //23hrs
 
-MPI_Comm single_server_comm;
-
-struct timespec ts = { 0, 100000000 };
-int cupoFeria = 100;
+	struct timespec ts = {0, 100000000L };
+	int cupoFeria = 100;
 
 ////DECLARACIÓN DE FUNCIONES////
 
-int generarHoraLlegada() {
+int generarHoraLlegada(){
 
 	srand(time(NULL));
 
@@ -34,21 +25,21 @@ int generarHoraLlegada() {
 
 }
 
-int dondeIr() { //genera la descición de la gente de a donde ir
+int dondeIr(){ //genera la descición de la gente de a donde ir
 	//1 PASILLO
 	//2 MOTANA
 	//3 CABALLO
 	//4 IRSE
 	srand(time(NULL));
-	int dondeIr = (rand() % 4) + 1;
+	int dondeIr = (rand()% 4) + 1;
 
 	return dondeIr;
 }
 
-void reloj(int tid, int hora) { //prueba para saber si un proceso recibe la hora
+void reloj(int tid, int hora){ //prueba para saber si un proceso recibe la hora
 
-	if (hora == 540 || hora == 1380)
-		printf("soy la persona %d hora %d:00\n", tid, hora / 60);
+	printf("Soy el proceso %d y la hora es %d \n",tid,hora/60);
+	//if(hora==540 || hora==1380)printf("soy la persona %d hora %d \n", tid, hora/60);
 
 	return;
 
@@ -62,45 +53,17 @@ void ocupado(int tiempo) {
 
 }
 
-void inicializaArreglo(int arreglo[], int tam) { //llena los arreglos con ceros
-
-	int i;
-	for (i = 0; i < tam; i++) {
-		arreglo[i] = 0;
-		printf("%d", arreglo[i]);
-
-	}
-}
-
-int arregloVacio(int arr[], int tam) { //1=arreglo vacio 0=arreglo con al menos 1 elemento
-
-	int i, vacio = 1;
-	for (i = 0; i < tam; i++) {
-		if (arr[i] != 0)
-			vacio = 0;
-	}
-	return vacio;
-}
-
-int arregloLleno(int arr[], int tam) { //1=arreglo lleno 0=arreglo con al menos 1 hueco
-
-	int i, lleno = 1;
-	for (i = 0; i < tam; i++) {
-		if (arr[i] == 0)
-			lleno = 0;
-	}
-	return lleno;
-}
-
 ////DECLARACIÓN DE FUNCIONES////
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[]){
 
-	int tid; /* rank of process */
-	int p; /* number of processes */
-	int horaActual;
 
-	MPI_Status status; /* return status for receive */
+	int  tid; /* rank of process */
+	int  p;       /* number of processes */
+	int horaActual = horaIncioDia;
+
+	MPI_Status status ;   /* return status for receive */
+
 
 	MPI_Init(&argc, &argv);
 
@@ -108,117 +71,98 @@ int main(int argc, char* argv[]) {
 
 	MPI_Comm_size(MPI_COMM_WORLD, &p);
 
-	//Tiempo
-	if (tid == 0) {
-		int horaActual;
 
-		for (horaActual = horaIncioDia; horaActual <= horaFinDia;
-				horaActual++) {
-			//horaActual=i;
-			MPI_Bcast(&horaActual, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	do{
+
+		if(tid == 0){	//Tiempo
+				MPI_Bcast(&horaActual,1,MPI_INT,0,MPI_COMM_WORLD);//manda la hora actual a todos los procesos
+				horaActual++;
 		}
-	}
 
-	do {
+		//Pasillos = 0  Tiempo de estancia en pasillos 2 min   TAG 0
+		if(tid == 1){
 
-		//Pasillos = 0  Tiempo de estancia en pasillos 2 min
-		if (tid == 1) {
-
-			int cupo[100];
-			inicializaArreglo(cupo, 100);
-
-			MPI_Bcast(&horaActual, 1, MPI_INT, 0, MPI_COMM_WORLD);
-			//MPI_Recv(cupo,1,MPI_INT,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,&status);
-
-		}
-		//Montana = 1 Duracion de juego 2 minutos
-		if (tid == 2) {
-			int fila = 0; //15
-			int cupo = 0; //8
-			//inicializaArreglo(fila, 15);
-			//inicializaArreglo(cupo, 8);
 
 			int clienteActual = 0;
 
-			MPI_Bcast(&horaActual, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-			MPI_Recv(&clienteActual, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG,
-			MPI_COMM_WORLD, &status);
+			MPI_Recv(&clienteActual, 1, MPI_INT, MPI_ANY_SOURCE, 100 ,MPI_COMM_WORLD, &status);
 
-			if (fila < 15)
-				fila++;
-			if (cupo < 8) {
-				cupo++;
-				fila--;
-			}
-			if (fila == 15) {
-				printf("|%d:%d| Soy persona %d y estan llena la fila de caballos\n",
-						horaActual / 60, horaActual % 60, clienteActual);
-				MPI_Send(&tid, 1, MPI_INT, 100, clienteActual, MPI_COMM_WORLD); //Libera, manda lavrg
+			printf("ME QUEDE ATORADO %d",tid);
 
-			}
-			if (cupo == 8) {
-				ocupado(2); //Inicia los caballitos
-				printf("|%d:%d|LOS CABALLOS INICIAN!!!!\n", horaActual / 60,
-						horaActual % 60);
+			MPI_Bcast(&horaActual,1,MPI_INT,0,MPI_COMM_WORLD);
+			//////////////////////////////////////PRUEBA/////////////////////////////////////////////////////////////////////
+			MPI_Send(&tid, 1, MPI_INT, 100, status.MPI_SOURCE , MPI_COMM_WORLD); //Libera, manda lavrg
 
-				MPI_Send(&tid, 1, MPI_INT, 100, clienteActual, MPI_COMM_WORLD); //Libera
-				cupo = 0;
-			}
+			reloj(tid,horaActual);
+			//
 
 		}
-		//caballo = 2   Duracion de 1 minuto
-		if (tid == 3) {
-		//	int fila[20];
-		//	int cupo[10];
-		//	inicializaArreglo(fila, 20);
-		//	inicializaArreglo(cupo, 10);
+		//Montana = 1     Duracion de juego 1 minuto
+		if(tid == 2){
+
+			int fila = 0; //15 fila
+		    int cupo = 0; //8 cupo
+
+
+			int clienteActual = 0;
+			MPI_Bcast(&horaActual,1,MPI_INT,0,MPI_COMM_WORLD);
+			MPI_Recv(&clienteActual, 1, MPI_INT, MPI_ANY_SOURCE, 101 ,MPI_COMM_WORLD, &status);
+			//////////////////////////////////////PRUEBA/////////////////////////////////////////////////////////////////////
+			MPI_Send(&tid, 1, MPI_INT, 101, clienteActual, MPI_COMM_WORLD); //Libera, manda lavrg
+
+			reloj(tid,horaActual);
+		}
+		//caballo = 2  duracion 2 minutos
+		if(tid == 3){
 			int fila = 0; //20
 			int cupo = 0; //10
 
-			int clienteActual;
+			int clienteActual = 0;
+			MPI_Bcast(&horaActual,1,MPI_INT,0,MPI_COMM_WORLD);
+			MPI_Recv(&clienteActual, 1, MPI_INT, MPI_ANY_SOURCE, 102 ,MPI_COMM_WORLD, &status);
 
-			//MPI_Recv(clienteActual,1,MPI_INT,MPI_ANY_SOURCE,MPI_ANY_TAG,newcomm,status);
+//////////////////////////////////////PRUEBA/////////////////////////////////////////////////////////////////////
+			MPI_Send(&tid, 1, MPI_INT, 102, clienteActual, MPI_COMM_WORLD); //Libera, manda lavrg
 
-			MPI_Bcast(&horaActual, 1, MPI_INT, 0, MPI_COMM_WORLD);
-			MPI_Recv(&clienteActual, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG,
-			MPI_COMM_WORLD, &status);
-
-			if (fila < 20)
-				fila++;
-			if (cupo < 10) {
-				cupo++;
-				fila--;
-			}
-			if (fila == 20) {
-				MPI_Send(&tid, 1, MPI_INT, 100, clienteActual, MPI_COMM_WORLD); //Libera, manda lavrg
-				printf("|%d:%d| Soy persona %d y esta llena la fila de los caballos \n",
-						horaActual / 60, horaActual % 60, tid);
-
-			}
-			if (cupo == 10) {
-				ocupado(1); //Inicia los caballitos
-				printf("|%d:%d|LOS CABALLOS INICIAN YA!!!!\n", horaActual / 60,
-						horaActual % 60);
-				MPI_Send(&tid, 1, MPI_INT, 100, clienteActual, MPI_COMM_WORLD); //Libera
-				cupo = 0;
-			}
-
+			reloj(tid,horaActual);
 		}
 		//Todos los demas
-		else {
+		if(tid  >= 4){
 
 			int horaLlegada = generarHoraLlegada();
 			int queHacer;
+			int tid_pasillos=0;
 
-			MPI_Bcast(&horaActual, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-			if (cupoFeria > 0) {
+			MPI_Bcast(&horaActual,1 ,MPI_INT,0,MPI_COMM_WORLD);
+
+			printf("sali de los 3 juegos soy la persona %d\n",tid);
+
+////////////////////////////////////////////AQUI SE ROMPE//////////////////////////////////////////////////////////////////////////
+			MPI_Send(&tid, 1, MPI_INT, 1, 100,MPI_COMM_WORLD); //pasillos  0
+			printf("ya mande a 1");
+			MPI_Recv(&tid_pasillos, 1, MPI_INT, MPI_ANY_SOURCE,100,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			tid_pasillos=0;
+			printf("SALI DEL PRIMER SEND  %d \n",tid);
+
+			MPI_Send(&tid, 1, MPI_INT, 2, 101,MPI_COMM_WORLD); // Rusa   1
+			MPI_Recv(&tid_pasillos, 1, MPI_INT, MPI_ANY_SOURCE,101,	MPI_COMM_WORLD, &status);
+			tid_pasillos=0;
+			printf("SALI DEL SEGUNDO SEND  %d \n",tid);
+
+			MPI_Send(&tid, 1, MPI_INT, 3, 102,MPI_COMM_WORLD); //caballos   2
+			MPI_Recv(&tid_pasillos, 1, MPI_INT, MPI_ANY_SOURCE,102,MPI_COMM_WORLD, &status);
+
+			printf("sali de los 3 juegos %d\n",tid);
+			//reloj(tid,horaActual);
+
+		/*	if(cupoFeria > 0){
 				cupoFeria--;
-				if (horaLlegada == horaActual) { //Llego a la feria
-					do {
+				if(horaLlegada == horaActual){ //Llego a la feria
+					do{
 						queHacer = dondeIr(); //Decido que hacer
-						printf("Voy a ir %d\n", queHacer);
+						printf("Voy a ir %d\n",queHacer);
 
 						if (queHacer != 4) {//Evita que mande mensaje a tid = 4 y se rompa esta madre
 							MPI_Send(&tid, 1, MPI_INT, queHacer, 100,MPI_COMM_WORLD);
@@ -226,17 +170,22 @@ int main(int argc, char* argv[]) {
 							MPI_Recv(&tid, 1, MPI_INT, MPI_ANY_SOURCE,MPI_ANY_TAG,
 									MPI_COMM_WORLD, &status);
 						}
-					} while (queHacer != 4);
+						break;
+					}while(queHacer != 4);
 					cupoFeria++; //Estoy saliendo de la feria
 				}
-			} else {
-				//Codigo que hace que espere. No hay cupo en la feria
-			}
+			}else{
+				//Codigo que hace que espere
+			} */
 		}
 
-	} while (horaActual < horaFinDia);
+		//MPI_Barrier(MPI_COMM_WORLD);
+
+
+	}while(horaActual< horaFinDia);
 
 	MPI_Finalize();
+
 
 	return 0;
 
